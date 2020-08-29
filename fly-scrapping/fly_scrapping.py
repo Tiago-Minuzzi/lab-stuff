273882259c09c144dda31f79f0a342064f5dbcc6
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2020-08-29 08:46:57 -0300

    fly_scrapping minor changes

--- fly-scrapping/fly_scrapping.py
+++ fly-scrapping/fly_scrapping.py
@@ -2,26 +2,16 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# Target site
 fly_batch = "https://flybase.org/batchdownload"
-chrdrv = "/home/tiago/.local/bin/chromedriver"
+# Browser driver
+drv = "/home/tiago/.local/bin/chromedriver"
+# Page
 results_to = "/html/body/div[2]/div/div/form/fieldset/div[2]/div[1]/div[2]/div[3]/div/select/option[2]"
-next_button = "//*[@id="submit"]"
 
-driver = webdriver.Chrome(chrdrv)
+driver = webdriver.Chrome(drv)
 driver.get(fly_batch)
 search_box = driver.find_element_by_xpath('//*[@id="ids"]')
 search_box.send_keys("FBgn0191532")
 #search_box.submit()
 driver.quit()
-
-
-#gecko="/usr/bin/geckodriver"
-#
-#cap = DesiredCapabilities().FIREFOX
-#cap["marionette"] = False
-#
-#firefox = webdriver.Firefox(capabilities=cap, executable_path=gecko)
-##firefox.get("https://flybase.org/batchdownload")
-#firefox.get("https://google.com.br")
-
-#firefox.quit()
