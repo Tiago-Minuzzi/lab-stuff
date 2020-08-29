e26dd69f7c320c1626a49ca381910b5409cf7315
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2020-08-29 16:10:26 -0300

    fly_scrapping alpha version

--- fly-scrapping/fly_scrapping.py
+++ fly-scrapping/fly_scrapping.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 import time
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import Select
+
+# File
+arquivo_ids = "ids.test.csv" 
+with open(arquivo_ids,"r") as arq_ids:
+    arq_ids = arq_ids.read()
+
 # Target site
 fly_batch = "https://flybase.org/batchdownload"
+FEATURES = ("SYMBOL",
+            "FEATURE_TYPE",
+            "UNIPROT_PROTEIN_FAMILY",
+            "GO_MOLECULAR_FUNCTION",
+            "GO_BIOLOGICAL_PROCESS",
+            "GO_CELLULAR_COMPONENT",
+            "DROSOPHILA_ORTHOLOGS")
+
 # Browser driver
 drv = "/home/tiago/.local/bin/chromedriver"
-# Page
-results_to = "/html/body/div[2]/div/div/form/fieldset/div[2]/div[1]/div[2]/div[3]/div/select/option[2]"
-
 driver = webdriver.Chrome(drv)
 driver.get(fly_batch)
-search_box = driver.find_element_by_xpath('//*[@id="ids"]')
-search_box.send_keys("FBgn0191532")
-#search_box.submit()
+
+# FIRST PAGE
+# Fill box with ids from file
+ids_box = driver.find_element_by_xpath('//*[@id="ids"]')
+ids_box.send_keys(arq_ids)
+# Select type of output (either file or browser)
+select = Select(driver.find_element_by_id("output"))
+select.select_by_value("File")
+# Click sumbmit button
+submeter = driver.find_element_by_id("submit") 
+submeter.click()
+
+# SECOND PAGE
+for FTR in FEATURES:
+    FT = driver.find_element_by_id(FTR) 
+    FT.click()
+
+# Get table
+get_table = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/input[1]")
+get_table.click()
+
+# Quit browser (to close tab only, use driver.close())
+time.sleep(1)
 driver.quit()
