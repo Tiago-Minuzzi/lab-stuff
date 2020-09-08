e93e18b6fad0de6f184deb8902c0970a59d9c971
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2020-09-08 16:51:40 -0300

    Get fasta files definition

--- get_fasta_seq.py
+++ get_fasta_seq.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # Created by Tiago Minuzzi, 2020.
 
+# Files
+in_fasta = "teste.fasta"
+in_ids = "ids.txt"
+out_fasta = "res_fas.fa"
 # Empty dictionary to store sequences
 fas_dict = {}
 # Read reference fasta file
-with open("teste.fasta", "r") as fasta:
+with open(in_fasta, "r") as fasta:
 	fasta=fasta.readlines()
 	for linha in fasta:
 		linha = linha.strip()
@@ -14,7 +18,7 @@ with open("teste.fasta", "r") as fasta:
 		else:
 			fas_dict[kl] = linha.upper()
 # Open ids file and create output file
-with open("ids.txt","r") as ides, open("res_fas.fa",'w') as fas_out:
+with open(in_ids,"r") as ides, open(out_fasta,'w') as fas_out:
 	ides = ides.read()
 	for sid, sseq in fas_dict.items():
 		# Search ids from dict in ids file
