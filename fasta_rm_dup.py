e21a8714a59fa3b4ce924bb06ce6e1cc6bc82667
Tiago-Minuzzi <tiago.minuzzi87@google.com>
2020-10-20 20:48:27 -0300

    Save duplicate sequences also

--- fasta_rm_dup.py
+++ fasta_rm_dup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Remove duplicated sequences in fasta file
-# This script removes duplicated sequences even though they have different id's.
 # Usage: python3 fasta_rm_dup.py your_file.fasta
+# Input file and script must be in the same folder
 
 import sys
 from Bio import SeqIO
@@ -9,16 +9,21 @@ from Bio import SeqIO
 in_fas = sys.argv[1]
 seen = []
 records = []
-# Read fasta and append to lists
+dup_seen = []
+dup_records = []
+# examples are in sequences.fasta
 for record in SeqIO.parse(in_fas, "fasta"):
     if str(record.seq) not in seen:
         seen.append(str(record.seq))
         records.append(record)
-
+    else:
+        dup_seen.append(str(record.seq))
+        dup_records.append(record)
 # printing to console
 # for record in records:
 #     print(f'>{record.name}')
 #     print(record.seq)
 
-# Write to new fasta file
+# writing to a fasta file
 SeqIO.write(records, f"unique_{in_fas}", "fasta")
+SeqIO.write(dup_records, f"dup_{in_fas}", "fasta")
