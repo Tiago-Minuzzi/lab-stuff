85ddf25801ce417b79318b4ab8bda775efd4869f
Tiago-Minuzzi <tiago.minuzzi87@google.com>
2020-10-27 22:07:40 -0300

    Improved version of rep_scrap

--- rep_scrap.py
+++ rep_scrap.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 # Save repbase repeat masking results
-# Replace the url with your results url
+# Requirements: python3, requests and BeatifulSoup
+# Install BeautifulSoup via pip
 # Usage: python3 rep_scrap.py
 import requests
 from bs4 import BeautifulSoup as soup
-# Insert repbase results URL here
-url = 'https://www.girinst.org/cgi-bin/censor/show_results.cgi?id=28590&lib=root'
-# Request page and fetch results
-page = requests.get(url)
-sp = soup(page.content, 'html.parser')
-tables = sp.findAll("table")
-# Calculate size to extract unique results
-size = int(len(tables[:-1])/2-1)
+# Prompt user for url and file name to save results
+url = input("Paste repbase results url: ")
+csv_out = input("Name for the output file: ")
 # Header
 HEADER = "Name,From,To,Name,From,To,Class,Dir,Sim,Pos/Mm:Ts,Score"
-# Print results
-print(HEADER)
-for table in tables[:size]:
-    table = table.get_text().replace(' ',',')
-    results = table[57:]
-    print(results)
+# Insert repbase results URL here
+# url = 'https://www.girinst.org/cgi-bin/censor/show_results.cgi?id=10762&lib=root'
+# Request page and fetch results
+page = requests.get(url)
+# Store the contents of the website
+sp = soup(page.content, 'html.parser')
+# Find tables
+tables = sp.findAll('table')
+size = len(tables[:-1]) # remove final statistics
+# Get tables
+rep_list = []
+for table in range(size):
+    for row in tables[table].find_all("tr")[1:]:
+        row = row.get_text().replace(' ',',')
+        rep_list.append(row)
+# Get main results
+res_size = int(len(rep_list)/2)
+# Save results to file
+with open(csv_out,"w") as rep_out:
+    rep_out.write(HEADER+'\n')
+    for item in rep_list[:res_size]:
+        rep_out.write(item+"\n")
