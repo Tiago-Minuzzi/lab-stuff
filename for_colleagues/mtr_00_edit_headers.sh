5d9665c420234f68c36a2a55da360ebbd303bf23
Tiago-Minuzzi <tiago.minuzzi87@google.com>
2020-12-01 14:19:16 -0300

    Replace gapopen for qcov

--- for_colleagues/mtr_00_edit_headers.sh
+++ for_colleagues/mtr_00_edit_headers.sh
@@ -7,7 +7,7 @@ SIGNP="sinal_proteico.tabular"
 WPSRT="wolf_loc_cell.tabular"
 # Editar headers
 sed -i 's/# target name/target_name/;s/ of /_of_/;s/query name/ID/;/#/d' $HMMER
-sed -i "1 i ID\tsseqid\tpident\tlength\tmismatch\tgapopen\tqstart\tqend\tsstart\tsend\tevalue\tbitscore" $BLAST
+sed -i "1 i ID\tsseqid\tpident\tlength\tmismatch\tqcov\tqstart\tqend\tsstart\tsend\tevalue\tbitscore" $BLAST
 sed -i "s/#Identifier/ID/" $PROMO
 sed -i "s/^#//" $SIGNP
 sed -i "s/^#//" $WPSRT
