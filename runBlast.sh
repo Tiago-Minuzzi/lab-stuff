c826bc8ea6d2708e8def8069032256e475b25f13
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-01-27 12:02:35 -0300

    Usage info

--- runBlast.sh
+++ runBlast.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+# USAGE
+# on command line run:
+# bash runBlast.sh query.fasta subject.fasta blast_output.tsv
+# Set blast output format
+OUTFMT="6 qseqid sseqid qlen slen length qcovhsp evalue bitscore qseq sseq"
+# Set max_target_seqs (MTGSEQS) and max_hsps (MHSPS)
+MTGSEQS=5
+MHSPS=5
 # User input
 QUERY=$1
 SUBJC=$2
 OUT=$3
-# Set max_target_seqs (MTGSEQS) and max_hsps (MHSPS)
-MTGSEQS=5
-MHSPS=5
-# Set blast output format
-OUTFMT="6 qseqid sseqid qlen slen length qcovhsp evalue bitscore qseq sseq"
 # Run blast
 if [[ -n $QUERY ]] && [[ -n $SUBJC ]] && [[ -n $OUT ]]; then # '-n' checks if var is not null.
 blastn -query $QUERY -subject $SUBJC -max_target_seqs $MTGSEQS -max_hsps $MHSPS -outfmt "$OUTFMT" -out $OUT
