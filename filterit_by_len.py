2e723fe0cb44b241c3609a3ae850389287ce2d87
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-03-18 17:44:22 -0300

    Filter sequences iteractive mode

---
+++ filterit_by_len.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from Bio import SeqIO
+
+SCRIPT = sys.argv[0]
+CWD = os.getcwd()
+
+class Inspetor:
+    def __init__(self,arquivo):
+        self.arquivo = arquivo
+        self.diretorio = os.path.dirname(self.arquivo)
+        self.caminho = os.path.abspath(self.diretorio)
+        self.nome = os.path.basename(self.arquivo)
+        self.novo = f'filtrado_{self.nome}'
+        self.saida = os.path.join(self.caminho, self.novo)
+
+        with open(self.arquivo) as arq_in:
+            arq_in = arq_in.readlines()
+            self.palavras = [len(linha.strip()) for linha in arq_in if not linha.startswith('>')]
+            self.def_menor = min(self.palavras)
+            self.def_maior = max(self.palavras)
+
+
+if __name__ == '__main__':
+    try:
+        ARQUIVO = sys.argv[1]
+        valor = Inspetor(ARQUIVO)
+        print(f'''
+# Select Sequences with desired lengths.
+# Let it empty for default values.
+# Defaults: min = {valor.def_menor}, max = {valor.def_maior}.
+                ''')
+        with open(valor.arquivo) as fasin, open(valor.saida, 'w') as fasout:
+            menor = int(input('- Enter min length: ') or valor.def_menor)
+            maior = int(input('- Enter max length: ') or valor.def_maior)
+            for record in SeqIO.parse(fasin, 'fasta'):
+                seq_len = len(record.seq)
+                if menor <= seq_len <= maior:
+                    SeqIO.write(record, fasout, 'fasta-2line')
+            if valor.caminho == CWD:
+                print(f'Saved as {valor.novo}.')
+            else:
+                print(f'Saved in {valor.caminho} as {valor.novo}.')
+    except IndexError:
+        print('# No file entered.\n# Please enter file name/location.')
+        print('# Run:')
+        print(f'python3 {SCRIPT} file.fasta')
+    except FileNotFoundError:
+        print(f"File '{ARQUIVO}' not found.")
+    except KeyboardInterrupt:
+        print('\n\nEnded by user.')
+        print('Bye, bye.')
