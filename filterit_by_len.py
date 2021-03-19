692f7d638d60d86e1255b40ed66756b4a01d6700
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-03-19 10:19:30 -0300

    Comments added

--- filterit_by_len.py
+++ filterit_by_len.py
@@ -3,22 +3,23 @@
 import os
 import sys
 from Bio import SeqIO
-
+# Vars for error handling and comparison
 SCRIPT = sys.argv[0]
 CWD = os.getcwd()
-
+# Class to get file info and create default values
 class Inspetor:
     def __init__(self,arquivo):
+        # Get file info
         self.arquivo = arquivo
         self.diretorio = os.path.dirname(self.arquivo)
         self.caminho = os.path.abspath(self.diretorio)
         self.nome = os.path.basename(self.arquivo)
         self.novo = f'filtered_{self.nome}'
-
+        # Create default values
         with open(self.arquivo) as arq_in:
             self.palavras = []
             for record in SeqIO.parse(arq_in, 'fasta'):
-                 
+                # Append sequence lengths to list 
                 self.palavras.append(len(record.seq))
             self.palavras = set(self.palavras)
             self.def_menor = min(self.palavras)
@@ -27,7 +28,9 @@ class Inspetor:
 
 if __name__ == '__main__':
     try:
+        # Get input from prompt
         ARQUIVO = sys.argv[1]
+        # Initialize class
         valor = Inspetor(ARQUIVO)
         print(f'''
 # Select Sequences with desired lengths.
@@ -36,9 +39,9 @@ if __name__ == '__main__':
 # Default name: {valor.novo}
                 ''')
         with open(valor.arquivo) as fasin:
-            menor = int(input('- Enter min length: ') or valor.def_menor)
-            maior = int(input('- Enter max length: ') or valor.def_maior)
-            novo = input('- Enter output file name: ') or valor.novo
+            menor = int(input('- Enter min length: ') or valor.def_menor) # set default min length
+            maior = int(input('- Enter max length: ') or valor.def_maior) # set default max length
+            novo = input('- Enter output file name: ') or valor.novo # set default file name
             fasout = os.path.join(valor.caminho, novo)
             with open(fasout, 'w') as fasout:
                 for record in SeqIO.parse(fasin, 'fasta'):
